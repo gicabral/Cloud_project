e52893871264bfe4f3e2816803fc460831f3b2c6
gicabral <38368320+gicabral@users.noreply.github.com>
2020-11-24 23:40:27 -0300

    falta o put pelo terminal

--- gicli.py
+++ gicli.py
@@ -6,7 +6,6 @@ import sys
 import json
 
 
-
 @click.command()
 @click.option('--listar', default=None, help='Coloque o DNS do load balancer para listar todas as tarefas')
 
@@ -15,31 +14,43 @@ def listar(listar):
     print(r.text)
 
 @click.command()
-@click.option('--adicionar', default=None, help='Coloque o DNS do load balancer para adicionar uma tarefa')
+@click.option('-a','--adicionar', default=None, help='Coloque o DNS do load balancer para adicionar uma tarefa')
+@click.option('-t', '--title', prompt=True, default=None, help='Coloque o titulo da tarefa')
+@click.option('-p', '--pub_date', prompt=True, default=None, help='Coloque a data da tarefa')
+@click.option('-d', '--description', prompt=True, default=None, help='Coloque a descrição da tarefa')
 
-def adicionar(adicionar):
-    with open("alter_tasks.json") as json_file:
-        data = json.load(json_file)
+
+def adicionar(adicionar, title, pub_date, description):
+    data = {
+        "title": title,
+        "pub_date": pub_date,
+        "description": description
+    }
     r = requests.post('http://'+adicionar+':8080/tasks/adicionar', json=data)
     print(r.text) 
 
 @click.command()
 @click.option('--deletar', default=None, help='Coloque o DNS do load balancer para deletar uma tarefa')
-@click.option('--id', default=None, help='Coloque o id da tarefa que quer deletar')
+@click.option('--id', prompt=True, default=None, help='Coloque o id da tarefa que quer deletar')
 
 def deletar(deletar, id):
-    with open("alter_tasks.json") as json_file:
-        data = json.load(json_file)
-    r = requests.delete('http://'+deletar+':8080/tasks/deletar/'+id, json=data)
+    r = requests.delete('http://'+deletar+':8080/tasks/deletar/'+id)
     print(r.text)  
 
 @click.command()
 @click.option('--atualizar', default=None, help='Coloque o DNS do load balancer para deletar uma tarefa')
-@click.option('--id', default=None, help='Coloque o id da tarefa que quer alterar')
+@click.option('--id', prompt=True, default=None, help='Coloque o id da tarefa que quer alterar')
+@click.option('--title', prompt=True, default=" ", help='Coloque o titulo da tarefa')
+@click.option('--pub_date', prompt=True, default=" ", help='Coloque a data da tarefa')
+@click.option('--description', prompt=True, default=" ", help='Coloque a descrição da tarefa')
 
-def atualizar(atualizar, id):
-    with open("update_tasks.json") as json_file:
-        data = json.load(json_file)
+def atualizar(atualizar, id, title, pub_date, description):
+    if title!= "" and pub_date != "" and description != "":
+        data = {
+            "title": title,
+            "pub_date": pub_date,
+            "description": description
+        }
     r = requests.put('http://'+atualizar+':8080/tasks/atualizar/'+id, json=data)
     print(r.text)            
 
